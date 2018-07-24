0a427f64256fe88a7b2e25a9dffac706472dca9e
amulyamari <amulya@saimail.com>
2018-07-24 15:47:02 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -6,4 +6,4 @@ meal_choice("eggs","pasta","rice")
 def meal_choice(a_breakfast="frosted flakes", a_lunch= "grilled choice",a_dinner= "salmon")
   puts "Hello, morning is the best time for #{a_breakfast} while #{a_lunch} is the best meal for lunch. Lastly, #{a_dinner} is the best for dinner!"
 end
-meal_choice("eggs", 'rice")
+meal_choice("eggs", "rice")
